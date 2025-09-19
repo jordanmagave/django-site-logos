acfaaa6d06d7095c2d08e4f47599967dfa46d5d5
jordanmagave <jordan.magave@celogos.com.br>
2025-09-19 17:56:00 -0300

    feat: atualizar footer com novos links e ajustar texto do botão de inscrição

--- fluxi/homeViews.py
+++ fluxi/homeViews.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 def index(request):
     data = {
         "header": "true",
+        "footer": "true",
+        "title": "Centro Educacional Logos - Formação Cristã Online",
     }
     return render(request, "home/index.html", data)
